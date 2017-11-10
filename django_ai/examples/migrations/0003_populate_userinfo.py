dcfb4269601c4a4d0d5dfeecb00c95857e79cbd1
Rodrigo <matematica.a3k@gmail.com>
2017-11-10 09:39:49 -0300

    [examples] Add support for table_size for UserInfo with the DJANGO_AI_EXAMPLES_USERINFO_SIZE settings variable

--- django_ai/examples/migrations/0003_populate_userinfo.py
+++ django_ai/examples/migrations/0003_populate_userinfo.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 import numpy as np
 
 from django.db import migrations
+from django.conf import settings
 
 
 def populate_userinfos(apps, schema_editor):
@@ -11,16 +12,17 @@ def populate_userinfos(apps, schema_editor):
     # Use a fixed seed for generate content
     np.random.seed(123456)
     # Size of table
-    size = 200
+    table_size = getattr(settings, "DJANGO_AI_EXAMPLES_USERINFO_SIZE", 200)
+    print(" (Table size is {})".format(table_size), end="")
     # Sex is ~ 70% F (0) / 30% M (1)
-    sex = np.random.binomial(1, 0.7, size)  # 200 Bernoullies :)
+    sex = np.random.binomial(1, 0.7, table_size)  # 200 Bernoullies :)
     # Age is around 30, mostly between 25 and 35
-    age = np.floor(np.random.normal(30, 2, size=(size,)))
+    age = np.floor(np.random.normal(30, 2, size=(table_size,)))
     # Average 1 is a metric normally distributed around 10 with a std dev of 5
-    avg1 = np.random.normal(10, 5, size=(size,))
+    avg1 = np.random.normal(10, 5, size=(table_size,))
     # Create the objects in the Model
     uis = []
-    for i in range(0, size):
+    for i in range(0, table_size):
         uis.append(UserInfo(age=age[i], sex=sex[i], avg1=avg1[i]))
     UserInfo.objects.bulk_create(uis)
 
