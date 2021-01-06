972bb69f505a293706871b09312e79d49a0268d6
Rodrigo <matematica.a3k@gmail.com>
2021-01-06 02:10:48 -0500

    Fix import path in supervised_learning

--- django_ai/supervised_learning/models/__init__.py
+++ django_ai/supervised_learning/models/__init__.py
@@ -5,4 +5,4 @@ import os
 if 'DJANGO_TEST' in os.environ:
     from django_ai.supervised_learning.models.svm import SVC
 else:
-    from supervised_learning.models.svm import SVC
+    from .svm import SVC
